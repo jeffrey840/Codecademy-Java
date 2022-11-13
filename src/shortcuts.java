5a356fbaf28afe6df2302e5100e81a7bbcb11c21
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-11-12 18:53:52 -0600

    commiting more code

--- src/shortcuts.java
+++ src/shortcuts.java
@@ -88,4 +88,90 @@ Main method finished
 
     */
 
+
+//                       Methods
+    /*
+
+
+       public class SavingsAccount {
+
+  int balance;
+
+  public SavingsAccount(int initialBalance){
+    balance = initialBalance;
+  }
+// check blalace method
+  public void checkBalance() {
+    System.out.println("Hello!");
+    System.out.println("Your balance is " + balance);
+  }
+// deposit method
+    public void deposit(int amountToDeposit) {
+      balance = balance + amountToDeposit;
+    System.out.println("You just deposited "+ amountToDeposit);
+  }
+
+  // withdraw method
+  public int withdraw(int amountToWithdraw) {
+      balance = balance - amountToWithdraw;
+      System.out.println("You just withdrew "+ amountToWithdraw);
+      return amountToWithdraw;
+  }
+
+  // to str
+  public String toString(){
+    return "The end";
+  }
+
+
+
+  public static void main(String[] args){
+    SavingsAccount savings = new SavingsAccount(2000);
+
+//Check balance:
+    savings.checkBalance();
+
+    //Withdrawing:
+    savings.withdraw(300);
+
+    //Check balance:
+    savings.checkBalance();
+
+    //Deposit:
+    savings.deposit(600);
+
+    //Check balance:
+    savings.checkBalance();
+
+    //Deposit:
+    savings.deposit(600);
+
+    //Check balance:
+    savings.checkBalance();
+
+    System.out.println(savings);
+  }
+}
+     */
+
+/*
+
+Prints:
+
+Hello!
+Your balance is 2000
+You just withdrew 300
+Hello!
+Your balance is 1700
+You just deposited 600
+Hello!
+Your balance is 2300
+You just deposited 600
+Hello!
+Your balance is 2900
+The end
+
+ */
+
+
 }
